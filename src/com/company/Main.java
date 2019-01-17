3df8ed567018657e8e38689bd66a1df4ae6e5c6a
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Air.local>
2019-01-17 16:43:45 -0500

    Add variable num to variable sum and print sum.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,10 @@ public class Main {
         int sum = 0;
         for (int num = 1; num <= 1000; num++) {
             if (num % 3 == 0 || num % 5 == 0) {
-
-
+                sum += num;
             }
         }
+        System.out.println(sum);
     }
 }
 
