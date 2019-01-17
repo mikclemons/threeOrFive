9ffc001f14d00b8f86c6d0dc126ba99535ef31c1
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Air.local>
2019-01-17 16:39:36 -0500

    Create a sum int.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        for (int multiple = 1; multiple < 1000; multiple++) {
-           System.out.println(multiple);
+
+        int sum = 0;
+        for (int num = 1; num <= 1000; num++) {
+            if (num % 3 == 0 || num % 5 == 0) {
+
 
             }
         }
     }
-
+}
 
 
